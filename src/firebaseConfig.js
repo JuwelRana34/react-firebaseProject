aa4feaa49cd08250ae845e9cfd5b48b3d3ccc009
JuwelRana34 <juwelrana3426@gmail.com>
2024-08-31 22:57:25 +0600

    done

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,26 +5,18 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from "firebase/storage";
 
 
-// const  firebaseConfig = {
-//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-//   projectId:  import.meta.env.VITE_FIREBASE_PROJECT_ID,
-//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-// };
+const  firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId:  import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
 
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDpt4Yh00hyP43taRy9QRsUajCvtsi1Z84",
-  authDomain: "juwelrana-7ec61.firebaseapp.com",
-  projectId: "juwelrana-7ec61",
-  storageBucket: "juwelrana-7ec61.appspot.com",
-  messagingSenderId: "528993110779",
-  appId: "1:528993110779:web:3161848e7eea218dcefe5e",
-  measurementId: "G-9XGHX7HGNQ"
-};
+
 
 
 
